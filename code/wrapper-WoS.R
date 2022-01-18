e0d91a82d209e9589f5d4095a15d05723f2f5c06
RichardLaBrie <richard.labrie90@gmail.com>
2022-01-18 17:38:20 -0500

    :wrench: New script for Web of Science

---
+++ code/wrapper-WoS.R
@@ -0,0 +1,50 @@
+### Wrapper for creating separate region-specific bibliographies 
+### Written by Britt Bertolet
+### 2021-12-09
+
+# Load libraries 
+library(stringr)
+library(tidyverse)
+library(ggplot2)
+library(cowplot)
+
+# Load function
+source('code/sortAffiliations-WoS.R')
+
+# Set variables needed to run function
+table=sortAffiliations(file="Marine-WoS.bib", n = 1000, GNout="Marine-WoS_GN.bib", GSout="Marine-WoS_GS.bib", dir="bibs")
+
+# Check for entries with multiple affiliations of the first author
+table[table$citation%in%table$citation[duplicated(table$citation)],]
+
+# Plot affiliations for all 890 citations
+(all890=ggplot(table, aes(x=country))+
+  geom_bar()+
+  facet_grid(.~designation, scales = "free_x", space = "free")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle=90, vjust=0.5),
+        panel.grid = element_blank(),
+        axis.title.x = element_blank())+
+  ylab("Count"))
+
+# Plot affiliations for only top 20 for both designations
+GN=table[table$designation =="Global North",]
+GN=GN[1:25,]
+GS=table[table$designation =="Global South",]
+GS=GS[1:25,]
+top25=rbind(GN, GS)
+
+top=ggplot(top25, aes(x=country))+
+  geom_bar()+
+  facet_grid(.~designation, scales = "free_x", space = "free")+
+  theme_bw()+
+  theme(axis.text.x = element_text(angle=90, vjust=0.5),
+        panel.grid = element_blank())+
+  ylab("Count")+
+  xlab("\nCountry of affiliation of first-author")
+
+plot_grid(all890, top, 
+          nrow=2, align = "h", 
+          labels=c("A", "B"))
+ggsave("D_scopus_affiliations.pdf", height=6, width=6)
+  
